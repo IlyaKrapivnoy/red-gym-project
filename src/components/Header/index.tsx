bbd6f1758e1541b15289f3a862348a9c3ae83cfd
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-08-27 00:07:09 +0300

    add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,21 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
-        <Toolbar>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
+        <Toolbar className='flex justify-between'>
+          <Image
+            className='cursor-pointer'
+            src='/logo.png'
+            alt='Picture of the author'
+            width={218}
+            height={28}
+          />
           <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
