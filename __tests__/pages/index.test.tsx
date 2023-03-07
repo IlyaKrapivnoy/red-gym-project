389db747bbd0af06c18f01ddba74c30703ec501d
Ilya Krapivnoy <ilya.krapivnoy@scelex.com>
2023-03-07 22:41:26 +0100

    fix unit tests

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Home from "../../pages";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../../pages';
+import '@testing-library/jest-dom';
 
-describe("Test Home", () => {
-  it("Home snapshot", () => {
+describe('Test Home', () => {
+  it('Home snapshot', () => {
     const view = render(<Home />);
     expect(view).toMatchSnapshot();
   });
